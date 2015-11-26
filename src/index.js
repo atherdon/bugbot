a495a0827e0d7b5ad96a10442b4b6f319013e7b7
brianleroux <b@brian.io>
2015-11-25 18:44:41 -0800

    better help

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import github from 'bugbot-github-issues'
 import slack, {slash, start} from './slack-bang-slash-hack'
 
+let help = `
+
+Welcome to Bugbot! 
+
+  /bb ..................... shows help
+  /bb auth ................ return your account info
+  /bb repo................. read your current repo
+  /bb repo org/reponame ... set current repo
+  /bb repo list ........... list your repos
+  /bb help ................ also shows help
+  /bb open ................ shows open issues
+
+To open a new Github Issue on the current repo:
+
+  /bb open your issue title here
+
+  And issue body here. Don't forget steps to reproduce!
+
+`
+
 // handler for the slack slash command: /bugbot
 slash('/bb', (payload, message)=> {
   if (payload.account.github_token) {
-    let text = JSON.stringify(payload)
+    let text = `${help} ${JSON.stringify(payload)}`
     message({text})
   }
   else {
