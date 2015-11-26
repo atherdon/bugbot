79a18a8279ed4626e0adf22a735b47a4442a6f06
brianleroux <b@brian.io>
2015-11-25 17:23:03 -0800

    refactor the lookup

--- src/slack-bang-slash-hack/routes.js
+++ src/slack-bang-slash-hack/routes.js
@@ -42,29 +42,18 @@ function index(req, res, next) {
 
 // recives a slash command
 function slash(req, res, next) {
-  let cmds = stack()
-  let sub = req.body.text? req.body.text.split(' ')[0] : ''
-  let cmd = `${req.body.command} ${sub}`.trim()
-  let ids = Object.keys(cmds)
-  let it = ids.filter(id=> id.indexOf(cmd) > -1)
+  let cmds = stack()                                         // all the commands
+  let sub  = req.body.text? req.body.text.split(' ')[0] : '' // sub command (foo from '/bb foo')
+  let cmd  = `${req.body.command} ${sub}`.trim()             // full command ('/bb foo')
+  let ids  = Object.keys(cmds)                               // all the command ids
+  let it   = ids.filter(id=> id.indexOf(cmd) > -1)           // array of matches
+  let id   = it.length === 0? ids[0] : it[0]                 // THE id or the first one
+  let msg  = cmds[id]                                        // the method to call
+  let done = msg=>res.status(200).json(msg)                  // the completion callback
 
-  if (it.length === 0) {
-    it = ids[0]
-  }
-  else {
-    it = it[0]
-  }
-
-  function done(msg) {
-    res.status(200).json(msg) 
-  }
-
+  // lookup the account, save if it doesn't exist, and pass the info to the callback hander
   let payload = req.body
-  payload.cmd = cmd
-  payload.ids = ids
-  cmds[it](payload, done)
-
-  //res.status(200).json({b:req.body, l:ids.length} )
+  msg(payload, done)
 }
 
 router.get('/', index)
