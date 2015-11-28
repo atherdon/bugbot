8a93bd478328df6d9d1aa45aff9967f7588a8909
brianleroux <b@brian.io>
2015-11-27 16:34:32 -0800

    team reg / app install: thru oauth flow only

--- src/slack-bang-slash-hack/methods/register.js
+++ src/slack-bang-slash-hack/methods/register.js
@@ -2,7 +2,7 @@ import save from './save'
 import whoami from './whoami'
 import api from './api'
 
-// registers an account
+// register app to a slack team
 export default function register(code, callback) {
   let url = 'https://slack.com/api/oauth.access'
   api(url, code, (err, json)=> {
@@ -10,15 +10,9 @@ export default function register(code, callback) {
       callback(err)
     }
     else {
-      let token = json.access_token
-      let scope = json.scope
-      // use the token to get the user account info
-      whoami(token, (err, acct)=> {
-        // save token and owner
-        acct.token = token
-        acct.owner = true
-        save(acct, callback)
-      })      
+      acct.token = json.access_token
+      acct.owner = true
+      save(acct, callback)
     }
   })
 }
