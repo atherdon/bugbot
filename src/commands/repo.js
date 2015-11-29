c03b0a71a3a0d024e8732db525fc61f91efa5bde
brianleroux <b@brian.io>
2015-11-28 21:42:18 -0800

    too many backticks freaks out babel

--- src/commands/repo.js
+++ src/commands/repo.js
@@ -31,17 +31,15 @@ export default function repoCmd(payload, message) {
   }
   else {
     let repo = payload.account.github_repo
-    let sets = '`/bb repo username/reponame`'
-    let list = '`/bb repos`'
     let text = repo? repo : `
         
 No repo set! Select a repo by running:
        
-  ${sets}
+  /bb repo username/reponame
 
 You can list all your repos by running:
 
-  ${list}
+  /bb repos
 
   `
   }
