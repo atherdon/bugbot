d20e803f02a4fe8c1e4b79b93861335d744cc430
brianleroux <b@brian.io>
2015-11-30 14:47:16 -0800

    adding auth txt

--- src/commands/auth.js
+++ src/commands/auth.js
@@ -12,8 +12,12 @@ export default function auth(payload, message, next) {
       let secret = process.env.SECRET
       let state  = {user_id:payload.account.user_id, team_id:payload.account.team_id}
       let token  = jwt.sign(state, secret) 
-      let text   = err? err: `<${link}&state=${token}|Please login with Github to use Bugbot>`
-      // let text   = `${err? err : anchor} \n\n\n ${JSON.stringify(payload)}`
+
+      let text   = err? err: `
+
+        Hey there! It looks like you haven’t authorized a Github account  yet. We need to do that in order to work with your Github Issues. Please <${link}&state=${token}|please authorize here>, and I’ll be ready for you when you’re back!
+        
+        `
       message({text})
     })
   }
