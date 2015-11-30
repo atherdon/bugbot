62a52711d910dbdea30b88928041dc4eef5773a5
brianleroux <b@brian.io>
2015-11-29 21:10:32 -0800

    omg typos

--- src/commands/issues.js
+++ src/commands/issues.js
@@ -3,7 +3,7 @@ import github from 'bugbot-github-issues'
 export default function repos(payload, message) {
   let token = payload.account.github_token
   let repo = payload.account.github_repo
-  github.issues(token, repo, (err, issuess)=> {
+  github.issues(token, repo, (err, issues)=> {
     let text = '```' + JSON.stringify(err? {err} : {issues}, null, 2) + '```'
     message({text})
   })
