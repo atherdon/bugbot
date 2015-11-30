a432cb3cf7e6a571bf6f99cb4ac2ae0d1d4ac4d4
brianleroux <b@brian.io>
2015-11-29 21:01:26 -0800

    this is how easy it is to add a command!

---
+++ src/commands/issues.js
@@ -0,0 +1,10 @@
+import github from 'bugbot-github-issues'
+
+export default function repos(payload, message) {
+  let token = payload.account.github_token
+  let repo = payload.account.github_repo
+  github.issues(token, repo, (err, issuess)=> {
+    let text = '```' + JSON.stringify(err? {err} : {issues}, null, 2) + '```'
+    message({text})
+  })
+}
--- src/slack.js
+++ src/slack.js
@@ -7,13 +7,15 @@ import whoami from './commands/whoami'
 import logout from './commands/logout'
 import repo from './commands/repo'
 import repos from './commands/repos'
+import issues from './commands/issues'
 
 // register slash command middlewares
 slash('/bb',        auth, help)
 slash('/bb whoami', auth, whoami)
 slash('/bb logout', auth, logout)
 slash('/bb repo',   auth, repo)
-slash('/bb repos',   auth, repos)
+slash('/bb repos',  auth, repos)
+slash('/bb open',   auth, issues)
 
 // if being called directly startup
 if (require.main === module) {
