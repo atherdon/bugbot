f86619d4e88a45e18ff753c6e091b7fda587a1ee
brianleroux <b@brian.io>
2015-12-12 10:58:04 -0800

    adding markdown formatting to message attachment for help

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,12 +4,14 @@ let attachments = [
   { 
     title: 'Adding and accessing your Github Issues', 
     color: '#2FA44F',
-    text: "`/bugbot add Your Issue Title` submit a new issue \n`/bugbot issues` show your open issues"
+    text: "`/bugbot add Your Issue Title` submit a new issue \n`/bugbot issues` show your open issues",
+    mrkdwn_in: ['text']
   },
   { 
     title: 'Configuring Bugbot', 
     color: '#E3E4E6',
-    text: "`/bugbot repo` display your current repo \n`/bugbot repo org/reponame` change your repo"
+    text: "`/bugbot repo` display your current repo \n`/bugbot repo org/reponame` change your repo \n`/bugbot repo` list your repos \n`/bugbot whoami` display your Github user \n`/bugbot help` you're lookin' at it! \n`/bugbot logout` revoke Bugbot's access to your Github",
+    mrkdwn_in: ['text']
   }
 ]
 
