2f1b7a555aed2e37f361a45ad8ca518d8e31f3ea
brianleroux <b@brian.io>
2015-12-14 15:24:52 -0800

    slack formatting the github issues and set the color to the first label

--- src/commands/issues.js
+++ src/commands/issues.js
@@ -3,8 +3,22 @@ import github from 'bugbot-github-issues'
 export default function repos(payload, message) {
   let token = payload.account.github_token
   let repo = payload.account.github_repo
+  let link = `<https://github.com/${repo}|${repo}>`
+  let text = `Here are the open issues for ${link}:`
+
   github.issues(token, repo, (err, issues)=> {
-    let text = '```' + JSON.stringify(err? {err} : {issues}, null, 2) + '```'
-    message({text})
+    // turn the github issues into slack attachments
+    let attachments = issues.map(i=> {
+      let color = i.labels && i.labels[0] ? i.labels[0].color : '#E3E4E6'
+      let mrkdwn_in = ['text']
+      return {
+        color,
+        mrkdwn_in,
+        title: i.title,
+        title_url: i.html_url
+      }
+    })
+    message({text, attachments})
   })
+///
 }
