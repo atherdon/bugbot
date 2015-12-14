342542552313e0af4187ea2e5a05fd4b2edd9d62
brianleroux <b@brian.io>
2015-12-14 13:18:48 -0800

    cleanup logout and repo set logic now works probably

--- src/commands/logout.js
+++ src/commands/logout.js
@@ -2,14 +2,12 @@ import slack from 'slack-express'
 import github from 'bugbot-github-issues'
 
 export default function logout(payload, message) {
-  let account = payload.account
-  delete account.github_token
-
   github.user(payload.account.github_token, (err, user)=> {
     if (err) {
       message({text:err})
     }
     else {
+      // yay message formatting
       let usr = '`'+ user.login +'`'
       let text = `Oh! It's cool, I understand. I've logged out ${usr}. Hope to see you soon!`
       let color = '#E3E4E6'
@@ -17,7 +15,10 @@ export default function logout(payload, message) {
       let author_name = user.login
       let author_link = user.html_url 
       let author_icon = user.avatar_url
-
+      // actually logout!
+      let account = payload.account
+      delete account.github_token
+      // saves the account without the github_token
       slack.save(account, err=> {
         message({attachments:[
           {text, color, mrkdwn_in, author_name, author_link, author_icon}
