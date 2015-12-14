8f6f4b238989b01e1ae20039e29e6b2a37626c07
brianleroux <b@brian.io>
2015-12-13 21:26:16 -0800

    more better-er whoami

--- src/commands/whoami.js
+++ src/commands/whoami.js
@@ -1,6 +1,21 @@
+import github from 'bugbot-github-issues'
+
 export default function whoami(payload, message) {
-  let text = `You're currently logged into Bugbot with Github user gracehopper`
-  let color = '#E3E4E6'
-  let mrkdwn_in = ['text']
-  message({attachments:[{text, color, mrkdwn_in}]})
+  github.user(payload.account.github_token, (err, user)=> {
+    if (err) {
+      message({text:err})
+    }
+    else {
+      let text = `You're currently logged into Bugbot with Github user /`${user.login}/``
+      let color = '#E3E4E6'
+      let mrkdwn_in = ['text']
+      let author_name = user.login
+      let author_link = user.html_url 
+      let author_icon = user.avatar_url
+
+      message({attachments:[
+        {text, color, mrkdwn_in, author_name, author_link, author_icon}
+      ]})
+    }
+  })
 }
