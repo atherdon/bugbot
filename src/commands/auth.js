61579b2537b33f4e0919d0889e2c575ecc0acd42
brianleroux <b@brian.io>
2015-12-13 18:11:21 -0800

    adding link for title using a property instead of message format bc oc

--- src/commands/auth.js
+++ src/commands/auth.js
@@ -14,9 +14,10 @@ export default function auth(payload, message, next) {
       let state  = {user_id:payload.account.user_id, team_id:payload.account.team_id}
       let token  = jwt.sign(state, secret) 
 
-      let color = '#2FA44F'
+      let color = '#E3E4E6'
       let mrkdwn_in = ['text']
-      let title = `<${link}&state=${token}|Uh oh, I need you to auth your Github account!>`
+      let title = 'Uh oh, I need you to auth your Github account!>'
+      let title_link = `${link}&state=${token}`
 
       let text = 'Hey there, It looks like you haven’t authorized a Github account yet! '
       text += 'I need to do that in order to work with your Github Issues. '
