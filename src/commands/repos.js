505d555962e605c52b39aa6afea246a7d5a16d9d
brianleroux <b@brian.io>
2015-12-13 21:41:42 -0800

    snazzy repos list

--- src/commands/repos.js
+++ src/commands/repos.js
@@ -2,8 +2,20 @@ import github from 'bugbot-github-issues'
 
 export default function repos(payload, message) {
   let token = payload.account.github_token
+  let text = "Here's a list of the Github repos I can use:\n" 
+  let color = '#E3E4E6'
+  let mrkdwn_in = ['text']
+  // get the repos
   github.repos(token, (err, repos)=> {
-    let text = '```' + JSON.stringify(err? {err} : {repos}, null, 2) + '```'
-    message({text})
+    // add the repos
+    repos.forEach(r=> {
+      text += `- ${r}\n`
+    })
+    text += 'To change to another repo just run this command:\n'
+    text += '`/bugbot repo org/reponame`'
+    // send the reponse msg
+    message({attachments:[
+      {text, color, mrkdwn_in}
+    ]})
   })
 }
